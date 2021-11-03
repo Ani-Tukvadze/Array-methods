88cdb283ce76ca2f5b2b78fc802d8112ab2c0ad5
Ani Tukvadze <ani.tukvadze14@geolab.edu.ge>
2021-11-03 12:10:02 -0800

    added examples

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+
+
+ let arr = ['javascript', 'php', 'c#', 'python', 'c++', 'kotlin', 'c', 'swift'];
+
+ for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length < 5) {
+           continue;    
+         }
+         console.log(arr[i]);
+        }
+
+
+
+
+ let arr = [2, -3, 5, 11, 1, -30, -11, 100, -1, -3, -4];
+
+ for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < 0) {
+           continue;
+        }
+        console.log(arr[i]);
+    }
+
+    
+
+
